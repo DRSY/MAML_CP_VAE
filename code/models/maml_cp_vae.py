75688bba3c5871e5cb422d3731788878727df604
roy <rsy0702@163.com>
2020-08-30 11:29:06 +0800

    .

--- code/models/maml_cp_vae.py
+++ code/models/maml_cp_vae.py
@@ -151,107 +151,108 @@ class MAMLCP_VAE(object):
         test_data = MonoTextData(test_data_pth, True, vocab=vocab)
         test_feat = np.load(test_feat_pth)
 
-        self.m.eval()
         train_data, train_feat = train_data.create_data_batch_feats(
             32, train_feat, device)
         print("Collecting training distributions...")
-        mus, logvars = [], []
-        step = 0
-        for batch_data, batch_feat in zip(train_data, train_feat):
-            mu1, logvar1 = self.m.lstm_encoder(batch_data)
-            mu2, logvar2 = self.m.mlp_encoder(batch_feat)
-            r, _ = self.m.mlp_encoder(batch_feat, True)
-            p = self.m.get_var_prob(r)
-            mu = torch.cat([mu1, mu2], -1)
-            logvar = torch.cat([logvar1, logvar2], -1)
-            mus.append(mu.detach().cpu())
-            logvars.append(logvar.detach().cpu())
-            step += 1
-            if step % 100 == 0:
-                torch.cuda.empty_cache()
-        mus = torch.cat(mus, 0)
-        logvars = torch.cat(logvars, 0)
-
-        neg_sample = dev_feat[:10]
-        neg_inputs = torch.tensor(
-            neg_sample, dtype=torch.float, requires_grad=False, device=device)
-        r, _ = self.m.mlp_encoder(neg_inputs, True)
-        p = self.m.get_var_prob(r).mean(0, keepdim=True)
-        neg_idx = torch.max(p, 1)[1].item()
-
-        pos_sample = dev_feat[-10:]
-        pos_inputs = torch.tensor(
-            pos_sample, dtype=torch.float, requires_grad=False, device=device)
-        r, _ = self.m.mlp_encoder(pos_inputs, True)
-        p = self.m.get_var_prob(r).mean(0, keepdim=True)
-        top2 = torch.topk(p, 2, 1)[1].squeeze()
-        if top2[0].item() == neg_idx:
-            print("Collision!!! Use second most as postive.")
-            pos_idx = top2[1].item()
-        else:
-            pos_idx = top2[0].item()
-        other_idx = -1
-        for i in range(3):
-            if i not in [pos_idx, neg_idx]:
-                other_idx = i
-                break
-
-        print("Negative: %d" % neg_idx)
-        print("Positive: %d" % pos_idx)
-
-        sep_id = -1
-        for idx, x in enumerate(test_data.labels):
-            if x == 1:
-                sep_id = idx
-                break
-
-        bsz = 64
-        ori_logps = []
-        tra_logps = []
-        pos_z2 = self.m.mlp_encoder.var_embedding[pos_idx:pos_idx + 1]
-        neg_z2 = self.m.mlp_encoder.var_embedding[neg_idx:neg_idx + 1]
-        other_z2 = self.m.mlp_encoder.var_embedding[other_idx:other_idx + 1]
-        _, d0 = get_coordinates(pos_z2[0], neg_z2[0], other_z2[0])
-        ori_obs = []
-        tra_obs = []
-        o_pth = f"../output/{self.mconf.corpus}/t{task_id}"
-        if not os.path.exists(os.path.dirname(o_pth)):
-            os.mkdir(os.path.dirname(o_pth))
-        with open(o_pth + "_generated_results.txt", "w") as f:
-            idx = 0
+        self.m.eval()
+        with torch.no_grad():
+            mus, logvars = [], []
             step = 0
-            n_samples = len(test_data.labels)
-            while idx < n_samples:
-                label = test_data.labels[idx]
-                _idx = idx + bsz if label else min(idx + bsz, sep_id)
-                _idx = min(_idx, n_samples)
-                var_id = neg_idx if label else pos_idx
-                text, _ = test_data._to_tensor(
-                    test_data.data[idx:_idx], batch_first=False, device=device)
-                feat = torch.tensor(
-                    test_feat[idx:_idx], dtype=torch.float, requires_grad=False, device=device)
-                z1, _ = self.m.lstm_encoder(text[:min(text.shape[0], 10)])
-                ori_z2, _ = self.m.mlp_encoder(feat)
-                tra_z2 = self.m.mlp_encoder.var_embedding[var_id:var_id + 1, :].expand(
-                    _idx - idx, -1)
-                texts = self.m.decoder.beam_search_decode(z1, tra_z2)
-                for text in texts:
-                    f.write("%d\t%s\n" % (1 - label, " ".join(text[1:-1])))
-
-                ori_z = torch.cat([z1, ori_z2], -1)
-                tra_z = torch.cat([z1, tra_z2], -1)
-                for i in range(_idx - idx):
-                    ori_logps.append(cal_log_density(
-                        mus, logvars, ori_z[i:i + 1].cpu()))
-                    tra_logps.append(cal_log_density(
-                        mus, logvars, tra_z[i:i + 1].cpu()))
-
-                idx = _idx
+            for batch_data, batch_feat in zip(train_data, train_feat):
+                mu1, logvar1 = self.m.lstm_encoder(batch_data)
+                mu2, logvar2 = self.m.mlp_encoder(batch_feat)
+                r, _ = self.m.mlp_encoder(batch_feat, True)
+                p = self.m.get_var_prob(r)
+                mu = torch.cat([mu1, mu2], -1)
+                logvar = torch.cat([logvar1, logvar2], -1)
+                mus.append(mu.detach().cpu())
+                logvars.append(logvar.detach().cpu())
                 step += 1
                 if step % 100 == 0:
-                    print(step, idx)
-        print("inference for corpus {}, task {} finished".format(
-            self.mconf.corpus, task_id))
+                    torch.cuda.empty_cache()
+            mus = torch.cat(mus, 0)
+            logvars = torch.cat(logvars, 0)
+
+            neg_sample = dev_feat[:10]
+            neg_inputs = torch.tensor(
+                neg_sample, dtype=torch.float, requires_grad=False, device=device)
+            r, _ = self.m.mlp_encoder(neg_inputs, True)
+            p = self.m.get_var_prob(r).mean(0, keepdim=True)
+            neg_idx = torch.max(p, 1)[1].item()
+
+            pos_sample = dev_feat[-10:]
+            pos_inputs = torch.tensor(
+                pos_sample, dtype=torch.float, requires_grad=False, device=device)
+            r, _ = self.m.mlp_encoder(pos_inputs, True)
+            p = self.m.get_var_prob(r).mean(0, keepdim=True)
+            top2 = torch.topk(p, 2, 1)[1].squeeze()
+            if top2[0].item() == neg_idx:
+                print("Collision!!! Use second most as postive.")
+                pos_idx = top2[1].item()
+            else:
+                pos_idx = top2[0].item()
+            other_idx = -1
+            for i in range(3):
+                if i not in [pos_idx, neg_idx]:
+                    other_idx = i
+                    break
+
+            print("Negative: %d" % neg_idx)
+            print("Positive: %d" % pos_idx)
+
+            sep_id = -1
+            for idx, x in enumerate(test_data.labels):
+                if x == 1:
+                    sep_id = idx
+                    break
+
+            bsz = 64
+            ori_logps = []
+            tra_logps = []
+            pos_z2 = self.m.mlp_encoder.var_embedding[pos_idx:pos_idx + 1]
+            neg_z2 = self.m.mlp_encoder.var_embedding[neg_idx:neg_idx + 1]
+            other_z2 = self.m.mlp_encoder.var_embedding[other_idx:other_idx + 1]
+            _, d0 = get_coordinates(pos_z2[0], neg_z2[0], other_z2[0])
+            ori_obs = []
+            tra_obs = []
+            o_pth = f"../output/{self.mconf.corpus}/t{task_id}"
+            if not os.path.exists(os.path.dirname(o_pth)):
+                os.mkdir(os.path.dirname(o_pth))
+            with open(o_pth + "_generated_results.txt", "w") as f:
+                idx = 0
+                step = 0
+                n_samples = len(test_data.labels)
+                while idx < n_samples:
+                    label = test_data.labels[idx]
+                    _idx = idx + bsz if label else min(idx + bsz, sep_id)
+                    _idx = min(_idx, n_samples)
+                    var_id = neg_idx if label else pos_idx
+                    text, _ = test_data._to_tensor(
+                        test_data.data[idx:_idx], batch_first=False, device=device)
+                    feat = torch.tensor(
+                        test_feat[idx:_idx], dtype=torch.float, requires_grad=False, device=device)
+                    z1, _ = self.m.lstm_encoder(text[:min(text.shape[0], 10)])
+                    ori_z2, _ = self.m.mlp_encoder(feat)
+                    tra_z2 = self.m.mlp_encoder.var_embedding[var_id:var_id + 1, :].expand(
+                        _idx - idx, -1)
+                    texts = self.m.decoder.beam_search_decode(z1, tra_z2)
+                    for text in texts:
+                        f.write("%d\t%s\n" % (1 - label, " ".join(text[1:-1])))
+
+                    ori_z = torch.cat([z1, ori_z2], -1)
+                    tra_z = torch.cat([z1, tra_z2], -1)
+                    for i in range(_idx - idx):
+                        ori_logps.append(cal_log_density(
+                            mus, logvars, ori_z[i:i + 1].cpu()))
+                        tra_logps.append(cal_log_density(
+                            mus, logvars, tra_z[i:i + 1].cpu()))
+
+                    idx = _idx
+                    step += 1
+                    if step % 100 == 0:
+                        print(step, idx)
+            print("inference for corpus {}, task {} finished".format(
+                self.mconf.corpus, task_id))
 
     def save_model(self, path):
         self.m.save_model(path)
