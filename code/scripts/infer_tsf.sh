17e7027b386b8ee89c00525f9a57d574b7c7fe7e
roy <rsy0702@163.com>
2020-08-31 11:10:25 +0800

    add device

--- code/scripts/infer_tsf.sh
+++ code/scripts/infer_tsf.sh
@@ -2,6 +2,6 @@ for s in 1 2
 do
 	for t in 1 2 3 4 5 6 7
 	do
-		python3 main.py --config-path ../config/s$s.json --corpus s$s --maml-epochs 0 --transfer-epochs 0 --task-id $t
+		python3 main.py --config-path ../config/s$s.json --corpus s$s --maml-epochs 0 --transfer-epochs 0 --task-id $t --device-index 0
 	done
 done
